2d93df37fda24105073c0597b140ea16109649d6
Aaleyah8 <aaleyahveness8@gmail.com>
2025-02-28 21:11:55 +0000

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -3,37 +3,49 @@
 #include "address.h"
 #include "student.h"
 
-void testAddress();
-void testDate();
-void testStudent();
+findStudent(std::vector<Student*>& students){
+	std::string target;
+	bool notFound = true;
+	std::cout << "last name of student ";
+	getline(std::cin, target);
+	for (student* s, students){
+		std::string 1Name = s->getLastName();
+		if (1Name.find (target)!= std::string::npos){
+			s->printStudent();
+			otFound = false;
+		} // end if
+	} // end for loop
+	if (notFound) {
+		std::cout << "Student " << target << " not found " << std::ends;
+	} // end if 
+} // end findsStudent
 
-int main(){
-  std::cout << "Hello!" << std::endl;
-  testAddress();
-  testDate();
-  testStudent();
-  return 0;
+bool compFirst(Student* a, Student* b){
+	return a->getFirstName() b->getFirstName();
+}
+printStudents(std::vector<Student*>& students)
+	getline(student, ss);
+
+showStudentNames(std::vector<Student*>& students){
+	getline(lastName, firstName);
+}
+
+loadStudents(std::vector<Student*>& students){
+	inFile.open("students.csv")
+}
+	
+
+delStudents(std::vector<Student*>& students){
+	for (Std::vector, students);
+		delete();
+}
+
+main(){
+	loadStudents(std::vector, students);
+	for (input == quit){	
+		std::cout "Print all student names ";
+		std::cout "print all student data ";
+		std::cout "find a student :;
+		ss.clear();
+	} // end for
 } // end main
-
-void testAddress(){
-  Address a;
-  a.init("123 W Main St", "Muncie", "IN", "47303");
-  a.printAddress();
-} // end testAddress
-
-void testDate(){
- Date d;
- d.init("01/27/1997");
- d.printDate();
-} // end testDate
-
-void testStudent(){
-  std::string studentString = "Danielle,Johnson,32181 Johnson Course Apt. 389,New Jamesside,IN,59379,02/17/2004,05/15/2027,65";
-  Student* student = new Student();
-  student->init(studentString);
-  student->printStudent();
-  std::cout << std::endl;
-  std::cout << student->getLastFirst();
-  delete student;
-} // end testStudent
-
